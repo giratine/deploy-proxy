bcf41e9cacd8adc8c1bb9299939976558dc272d3
giratine <hiepnguyennamlong@gmail.com>
2024-07-29 13:46:37 +0700

    six project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Swisstronik Tesnet Techinal Task 6 (Deploy Proxy)
+
+link : [Click!](https://www.swisstronik.com/testnet2/dashboard)
+
+
+
+
---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xAb9d0bc02733197b8f854353167568943C8c89DB'
+export const ProxyAdmin = '0xecc698e5699AC1d8656c5bA52Ea037a8266e22ED'
+export const SWTRImplementation = '0xF1acf7bD06B3E5fc1e4E80ACA764e9e62F4556b4'
+    
